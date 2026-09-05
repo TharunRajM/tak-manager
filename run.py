e7bd05c5206767c346830e350fbb9eb41beae4c7
TharunRajM <tharunrajmudavath@gmail.com>
2026-09-05 12:29:50 +0530

    Fix package structure for Vercel

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 
-# Add 'src' directory to Python module search path
 sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
 
 from file_upload import app
